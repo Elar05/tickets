ced33dfb1cafc99d3b4a6f557ac28c2d7b9532f9
Elar <elarlopezcortez6@gmail.com>
2024-11-02 18:15:28 -0500

    cambio de base de datos

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 define('URL', 'http://localhost/helpdeskpro');
 define('HOST', 'localhost');
-define('DB', 'instagram');
+define('DB', 'helpdeskpro');
 define('PASS', '');
 define('USER', 'root');
 define('CHARSET', 'utf8mb4');
