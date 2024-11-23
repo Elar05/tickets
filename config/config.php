150b879e060646663aa21af2b39685b02bf09c94
Elar <elarlopezcortez6@gmail.com>
2024-11-22 23:02:45 -0500

    config para pro

--- config/config.php
+++ config/config.php
@@ -1,9 +1,17 @@
 <?php
 
-define('URL', 'http://localhost/helpdeskpro');
+// define('URL', 'http://localhost/helpdeskpro');
+// define('HOST', 'localhost');
+// define('DB', 'helpdeskpro');
+// define('PORT', '3306');
+// define('PASS', '');
+// define('USER', 'root');
+// define('CHARSET', 'utf8mb4');
+
+define('URL', 'https://ticket.helppro.com.do');
 define('HOST', 'localhost');
-define('DB', 'helpdeskpro');
+define('DB', 'u359284306_helpdeskpro');
 define('PORT', '3306');
-define('PASS', '');
-define('USER', 'root');
+define('PASS', 'KJ6VWzmI2sJDVX');
+define('USER', 'u359284306_helpdeskpro');
 define('CHARSET', 'utf8mb4');
