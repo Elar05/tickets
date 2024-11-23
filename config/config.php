6cbf71bd5ec61de1de26f08246f4f94812185fb6
Elar <elarlopezcortez6@gmail.com>
2024-11-22 22:56:59 -0500

    add config gitignore

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('URL', 'https://ticket.helppro.com.do');
+define('URL', 'http://localhost/helpdeskpro');
 define('HOST', 'localhost');
-define('DB', 'u359284306_helpdeskpro');
+define('DB', 'helpdeskpro');
 define('PORT', '3306');
-define('PASS', 'KJ6VWzmI2sJDVX');
-define('USER', 'u359284306_helpdeskpro');
+define('PASS', '');
+define('USER', 'root');
 define('CHARSET', 'utf8mb4');
