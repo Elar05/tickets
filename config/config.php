414234fa8a1433ba06ef93c0bb771a5aa186e707
Elar <elarlopezcortez6@gmail.com>
2024-11-22 22:54:47 -0500

    varibales para producción

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('URL', 'http://localhost/helpdeskpro');
+define('URL', 'https://ticket.helppro.com.do');
 define('HOST', 'localhost');
-define('DB', 'helpdeskpro');
+define('DB', 'u359284306_helpdeskpro');
 define('PORT', '3306');
-define('PASS', '');
-define('USER', 'root');
+define('PASS', 'KJ6VWzmI2sJDVX');
+define('USER', 'u359284306_helpdeskpro');
 define('CHARSET', 'utf8mb4');
