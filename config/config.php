6933159f83b45c725f1d28c01f667d87a4ce533d
Elar <elarlopezcortez6@gmail.com>
2024-12-03 20:36:42 -0500

    update file config/config.php

--- config/config.php
+++ config/config.php
@@ -1,17 +1,16 @@
 <?php
 
+define('HOST', 'localhost');
+
 // define('URL', 'http://localhost/helpdeskpro');
-// define('HOST', 'localhost');
 // define('DB', 'helpdeskpro');
-// define('PORT', '3306');
 // define('PASS', '');
 // define('USER', 'root');
-// define('CHARSET', 'utf8mb4');
 
 define('URL', 'https://ticket.helppro.com.do');
-define('HOST', 'localhost');
 define('DB', 'u359284306_helpdeskpro');
-define('PORT', '3306');
 define('PASS', 'KJ6VWzmI2sJDVX');
 define('USER', 'u359284306_helpdeskpro');
+
+define('PORT', '3306');
 define('CHARSET', 'utf8mb4');
